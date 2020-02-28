f1c3e75b0ec04f6e432f13f01db28a8b629b5f0e
luiszimmermann <tato.zimmermann@gmail.com>
2020-02-28 17:19:17 -0300

    Bump Tomcat to 9.0.31

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 class TomcatGrailsPlugin {
-	def version = '9.0.30'
+	def version = '9.0.31'
 	def grailsVersion = '2.5 > *'
 	def scopes = [excludes: 'war']
 	def title = 'Apache Tomcat plugin'
